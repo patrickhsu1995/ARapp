a3e5bf5862348d6fec5e26746019ebdff3bbaed8
Patrick Hsu <patrick.hsu@mail.utoronto.ca>
2018-06-04 17:40:55 -0400

    Implemented feature for dropping item at user's current location

--- ARapp/ViewController.swift
+++ ARapp/ViewController.swift
@@ -12,35 +12,36 @@ import ARKit
 import GoogleMaps
 import ARCL
 import CoreLocation
+import Firebase
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+
+class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
     var sceneLocationView = SceneLocationView()
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var locationManager = CLLocationManager()
+    var currentCoordiantes: CLLocationCoordinate2D!
+    var currentAltitude: CLLocationDistance!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneLocationView.run()
         view.addSubview(sceneLocationView)
-//
-//        // Set the view's delegate
-//        sceneView.delegate = self
-//
-//        // Show statistics such as fps and timing information
-//        sceneView.showsStatistics = true
-//
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
-        let coordinate = CLLocationCoordinate2D(latitude: 43.642509, longitude: -79.387039)
-        let location = CLLocation(coordinate: coordinate, altitude: 570)
-        let image = UIImage(named: "pin")!
         
-        let annotationNode = LocationAnnotationNode(location: location, image: image)
-        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        //initialize location
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+        
+        //adding tap gesture to the view
         addTapGestureToSceneView()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        currentAltitude = locations.last?.altitude
+        currentCoordiantes = location
+        print(currentCoordiantes)
     }
     
     override func viewDidLayoutSubviews() {
@@ -84,46 +85,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 */
     //add a box to scene
-    func addBox() {
+    func addBox() -> SCNNode {
         let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
-//
-//        let boxNode = SCNNode()
-//        boxNode.geometry = box
-//        boxNode.position = SCNVector3(0, 0, -0.2)
-//        boxNode.name = "box"
-//
-        let coordinate = CLLocationCoordinate2D(latitude: 43.6426, longitude: -79.3871)
-        let location = CLLocation(coordinate: coordinate, altitude: 102)
-        
-        let annotationNode = LocationNode(location: location)
-        annotationNode.geometry = box
-        annotationNode.position = SCNVector3(0, 0, -0.2)
-        annotationNode.name = "box"
 
-        sceneView.scene.rootNode.addChildNode(annotationNode)
-        print(annotationNode.location!)
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(0, 0, -0.2)
+        boxNode.name = "box"
+        
+        return boxNode
     }
     
+    //create and drop a pin (tentative) at the user's height
     func dropItem(){
+        let objDropLocation = CLLocation(coordinate: currentCoordiantes, altitude: currentAltitude)
+        let image = UIImage(named: "pin")!
         
+        //create the object to be dropped
+        let dropObject = LocationAnnotationNode(location: objDropLocation, image: image)
+        
+        print("attempting to add object to current location")
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: dropObject)
     }
     
     func addTapGestureToSceneView(){
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
-        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        sceneLocationView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer){
-        let tapLocation = recognizer.location(in: sceneView)
-        let hitTestResults = sceneView.hitTest(tapLocation)
-        guard let node = hitTestResults.first?.node else {return}
-        node.removeFromParentNode()
-        if node.name == "box"{
-            let scene = SCNScene(named: "art.scnassets/ship.scn")!
-            sceneView.scene = scene
-        } else {
-            addBox()
-        }
+//        let tapLocation = recognizer.location(in: sceneView)
+//        let hitTestResults = sceneView.hitTest(tapLocation)
+//        guard let node = hitTestResults.first?.node else {return}
+//        node.removeFromParentNode()
+        dropItem()
+
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
--- Podfile
+++ Podfile
@@ -3,5 +3,8 @@ target 'ARapp' do
   pod 'GoogleMaps'
   pod 'GooglePlaces'
   pod 'ARCL'
+  pod 'Firebase'
   pod 'Firebase/Auth'
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
 end
